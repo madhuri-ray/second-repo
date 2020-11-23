97a7a6274cead456fd5dbf55d874342c5562987f
Shree <Shree@LAPTOP-VOD9JHKE>
2020-11-23 22:01:03 +0530

    my second repo

---
+++ src/secondPackage/MySecondClass.java
@@ -0,0 +1,18 @@
+package secondPackage;
+
+public class MySecondClass {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+	
+	
+	public void printMyName(String name)
+	{
+		System.out.println("Hello "+name);
+		
+	}
+
+}
